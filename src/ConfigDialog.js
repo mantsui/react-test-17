ee18f497994e4b10c2a5aee577cc7d7a2dca1bc5
Man H Tsui <mantsui@ucdavis.edu>
2020-03-05 21:12:26 -0800

    Update ConfigDialog.js

--- src/ConfigDialog.js
+++ src/ConfigDialog.js
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 import LineDropdown from './LineDropdown.js'
 import RadioButton from './RadioButton.js'
+
 
 let radioList = ['one', 'two', 'three'];
 //<RadioButton radioData={['one', 'two', 'three','four']} radioTitle='This is Radio Titlez'/>
 
-class ConfigDialog extends React.Component {
+const page3 = () => {
+    return ( 
+        <>
+        <h1>Tab 3!</h1>
+        <br/>
+        <RadioButton radioData={radioList} radioTitle='This is Radio 3a!'/>
+        <br/>
+        <RadioButton radioData={radioList} radioTitle='This is Radio 3b!'/>
+        </>
+    )
+}
+
+
+
+class ConfigDialog extends Component {
     render() {
         return(
-            <>
-            <h1>Tableau Extension D3 Sankey Diagram ConfigDialog.</h1>
+            <Router>
+                <div className="container">
+                <li><Link to="/">Home</Link></li>
+                <li> <Link to="/about">About</Link> </li>
+                <li> <Link to="/tab3">Page 3</Link> </li>
+                <li> <Link to="/tab4">Page 4</Link> </li>
+                <li> <Link to="/tab5">Page 5</Link> </li>
+                </div>
 
-            <LineDropdown />
-            <br/>
-            <RadioButton radioData={radioList} radioTitle='This is Radio Titlez'/>
-            </>
+                <Route path="/" exact strict render={
+                    ()=> { return ( <h1>Welcome Home</h1> ) }
+                }/>
+
+                <Route path="/about" exact strict render={
+                    ()=> { return ( <h1>Page 2</h1> ) }
+                }/>
+
+                <Route path="/tab3" exact strict component={page3} />
+
+                <Route path="/tab4" exact strict render={
+                    ()=> { return ( <LineDropdown /> ) }
+                }/>
+
+                <Route path="/tab5" exact strict render={
+                    ()=> {return( 
+                        <>
+                        <LineDropdown />
+                        <br/>
+                        <RadioButton radioData={radioList} radioTitle='This is Radio Titlez'/>
+                        </>
+                    )}
+                }/>
+
+            </Router>
         )
     }
 }
@@ -22,6 +66,15 @@ class ConfigDialog extends React.Component {
 export default ConfigDialog
 
 /*
+
+            <h1>Tableau Extension D3 Sankey Diagram ConfigDialog.</h1>
+
+            <LineDropdown />
+            <br/>
+            <RadioButton radioData={radioList} radioTitle='This is Radio Titlez'/>
+
+
+
 import React from 'react';
 import { Pill } from '@tableau/tableau-ui'
 import { DropdownSelect } from '@tableau/tableau-ui'
